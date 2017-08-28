7c7372da788db7e32a484951f28cecad9621eb2c
CapitalKnew <capitalknew@users.noreply.github.com>
2017-08-29 02:56:29 +0800

    opengl universe

--- include/diamond/opengl.hpp
+++ include/diamond/opengl.hpp
@@ -7,10 +7,14 @@
 #endif
 
 #include "glm/glm.hpp"
+#include <tuple>
 
 #define thisref (*this)
 
 namespace dgl {
+
+
+
     class base {
     protected:
         GLuint * globj = new GLuint[1];
@@ -22,6 +26,37 @@ namespace dgl {
         void allocate(const size_t cnt) { globj = new GLuint[cnt]; }
         operator const GLuint&() const { return (*globj); }
     };
+
+
+
+    // crop tuple element
+    template <class Tm, class... T, size_t... Is>
+    constexpr decltype(auto) get_gl_buf_reduce_impl(GLuint * rest, std::tuple<Tm, T...> &t, std::index_sequence<Is...>) {
+        *rest = (GLuint)std::get<0>(t); return std::make_tuple(std::get<Is + 1>(t)...);
+    }
+
+    template <class... T>
+    constexpr decltype(auto) get_gl_buf_reduce(GLuint * rest, std::tuple<T...> &t) {
+        return get_gl_buf_reduce_impl(rest, t, std::make_index_sequence<(sizeof...(T)) - 1>{});
+    }
+
+    // getting gl buffers
+    template <class T>
+    constexpr decltype(auto) get_gl_buf_impl(GLuint * ptr, std::tuple<T> &t) {
+        *ptr = (GLuint)std::get<0>(t);
+    }
+
+    template <class... T>
+    constexpr decltype(auto) get_gl_buf_impl(GLuint * ptr, std::tuple<T...> &t) {
+        decltype(auto) rest_tuple = get_gl_buf_reduce<T...>(ptr, t);
+        get_gl_buf_impl(ptr + 1, rest_tuple);
+    }
+
+    template <class... T>
+    constexpr decltype(auto) get_gl_buf(GLuint * ptr, std::tuple<T...> &t) {
+        return get_gl_buf_impl(ptr, t);
+    }
+
 
 
 };
